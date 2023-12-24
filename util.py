ac3f85f5ddc57af53d53d71f8f6dbc76f3a0202b
JanBergsma <bergsma_jan@hotmail.com>
2023-12-24 09:45:23 +0100

    reorg of files

---
+++ util.py
@@ -0,0 +1,10 @@
+def print_hex(hex: str, text: str) -> None:
+    r, g, b = tuple(int(hex[1:][i : i + 2], 16) for i in (0, 2, 4))
+    escape = "\033[{};2;{};{};{}m".format(38, r, g, b)
+    print(escape + text + "\033[0m")
+
+
+def hexcolor_str(hex: str, text: str) -> None:
+    r, g, b = tuple(int(hex[1:][i : i + 2], 16) for i in (0, 2, 4))
+    escape = "\033[{};2;{};{};{}m".format(38, r, g, b)
+    return escape + text + "\033[0m"
