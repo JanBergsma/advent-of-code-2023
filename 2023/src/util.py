92c2a624855976350f5b86b854195cdbddb45b05
JanBergsma <bergsma_jan@hotmail.com>
2024-01-02 10:35:22 +0100

    Day 22

--- 2023/src/util.py
+++ 2023/src/util.py
@@ -1,7 +1,7 @@
-def print_hex(hex: str, text: str) -> None:
+def print_hex(hex: str, text: str, end="\n") -> None:
     r, g, b = tuple(int(hex[1:][i : i + 2], 16) for i in (0, 2, 4))
     escape = "\033[{};2;{};{};{}m".format(38, r, g, b)
-    print(escape + text + "\033[0m")
+    print(escape + text + "\033[0m", end=end)
 
 
 def hexcolor_str(hex: str, text: str) -> None:
