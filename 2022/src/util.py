e75a47852e0fa1955696efecaa5410c56747aa1d
Jan Bergsma <bergsma_janhotmail.com>
2025-12-06 16:15:36 +0100

    temp

--- 2022/src/util.py
+++ 2022/src/util.py
@@ -8,3 +8,13 @@ def hexcolor_str(hex: str, text: str) -> str:
     r, g, b = tuple(int(hex[1:][i : i + 2], 16) for i in (0, 2, 4))
     escape = "\033[{};2;{};{};{}m".format(38, r, g, b)
     return escape + text + "\033[0m"
+
+
+class Print:
+
+    def __str__(self) -> str:
+        return f"{type(self).__name__}({
+            ', '.join(f'{n}={v}' for n, v in vars(self).items())
+        })"
+
+    __repr__ = __str__
