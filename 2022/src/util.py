c6a40be064414ced83a740f17157260c256feefe
Jan Bergsma <bergsma_janhotmail.com>
2025-12-23 11:56:07 +0100

    back up

--- 2022/src/util.py
+++ 2022/src/util.py
@@ -1,3 +1,6 @@
+from collections.abc import Callable
+
+
 def print_hex(hex: str, text: str, end="\n") -> None:
     r, g, b = tuple(int(hex[1:][i : i + 2], 16) for i in (0, 2, 4))
     escape = "\033[{};2;{};{};{}m".format(38, r, g, b)
@@ -10,7 +13,7 @@ def hexcolor_str(hex: str, text: str) -> str:
     return escape + text + "\033[0m"
 
 
-class Print:
+class Str:
 
     def __str__(self) -> str:
         return f"{type(self).__name__}({
@@ -18,3 +21,25 @@ class Print:
         })"
 
     __repr__ = __str__
+
+
+def add_repr_str(
+    cls: type | None = None, *, repr: bool = True, st: bool = False
+) -> Callable[..., type] | type:
+    def wrapper(cls: type) -> type:
+        def f(self) -> str:
+            return f"{type(self).__name__}({
+                    ', '.join(f'{n}={v}' for n, v in vars(self).items())
+            })"
+
+        if repr:
+            cls.__repr__ = f
+        if str:
+            cls.__str__ = f
+
+        return cls
+
+    if cls is None:
+        return wrapper
+
+    return wrapper(cls)
